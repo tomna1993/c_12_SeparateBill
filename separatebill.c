ebfdb9acbece673ec75a4ee4c9656bb7c80f19f7
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-28 11:11:36 +0200

    Create function for calculation

--- separatebill.c
+++ separatebill.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <cs50.c>
 
+float half(float bill, float tax, int tip);
+
 int main(void)
 {
 	float bill = get_float("Bill before tax and tip: ");
 	float taxPercent = get_float("Sale tax percent: ");
 	int tipPercent = get_float("Tip percent: ");
 
+	// Print out the money to pay
+	printf("You will owe $%.2f each!\n", half(bill, taxPercent, tipPercent));
+}
+
+float half(float bill, float tax, int tip)
+{
 	// Bill calculation method is
 	// Bill + tax then add tip
+	bill += bill * (tax / 100);
 
-	bill += bill * (taxPercent / 100);
-
-	bill += bill * ((float)tipPercent / 100);
+	bill += bill * ((float)tip / 100);
 
 	// Separate the bill to half
-	float separatedBill = bill / 2;
-
-	// Print out the money to pay
-	printf("You will owe $%.2f each!\n", separatedBill);
+	return bill / 2;
 }
